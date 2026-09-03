4478d813d5277edb6a4a3d9f872bbd01b39fe510
root <root@localhost.localdomain>
2026-09-03 01:51:26 +0000

    fix: add icon field and toJson method to CategoryModel

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,13 +1,40 @@
-enum CategoryType { income, expense }
+enum CategoryType {
+  income,
+  expense,
+}
 
 class CategoryModel {
   final String id;
   final String name;
+  final String icon;
   final CategoryType type;
+  final String? color;
 
   CategoryModel({
     required this.id,
     required this.name,
+    required this.icon,
     required this.type,
+    this.color,
   });
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) {
+    return CategoryModel(
+      id: json['id'] ?? '',
+      name: json['name'] ?? '',
+      icon: json['icon'] ?? '',
+      type: json['type'] == 'income' ? CategoryType.income : CategoryType.expense,
+      color: json['color'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'icon': icon,
+      'type': type == CategoryType.income ? 'income' : 'expense',
+      'color': color,
+    };
+  }
 }
