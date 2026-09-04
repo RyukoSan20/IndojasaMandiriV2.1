d2fbbf5bf1e8a929fbbce32eed70e1bab013f96d
root <root@localhost.localdomain>
2026-09-04 01:36:23 +0000

    feat: integrate frontend UI with reactive state management, currency formatting, and persistent local data

---
+++ lib/utils/currency_formatter.dart
@@ -0,0 +1,23 @@
+import 'package:intl/intl.dart';
+
+class CurrencyFormatterUtil {
+  static String format(double amount, {String currencyCode = 'IDR'}) {
+    switch (currencyCode) {
+      case 'USD':
+        return NumberFormat.currency(symbol: '\$', decimalDigits: 2).format(amount);
+      case 'EUR':
+        return NumberFormat.currency(symbol: '€', decimalDigits: 2).format(amount);
+      case 'JPY':
+        return NumberFormat.currency(symbol: '¥', decimalDigits: 0).format(amount);
+      case 'KRW':
+        return NumberFormat.currency(symbol: '₩', decimalDigits: 0).format(amount);
+      case 'SAR':
+        return NumberFormat.currency(symbol: 'SR ', decimalDigits: 2).format(amount);
+      case 'MYR':
+        return NumberFormat.currency(symbol: 'RM ', decimalDigits: 2).format(amount);
+      case 'IDR':
+      default:
+        return NumberFormat.currency(symbol: 'Rp ', decimalDigits: 0, locale: 'id_ID').format(amount);
+    }
+  }
+}
